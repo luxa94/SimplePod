1419e05b8de86511a32663e729aa4a03eb8b4332
Nikola Lukić <luknik94@gmail.com>
2018-04-20 11:36:10 +0200

    Asd

--- SimplePod/Classes/Something.swift
+++ SimplePod/Classes/Something.swift
@@ -3,12 +3,12 @@ public struct Something {
     var a: Int
     var b: String
 
-    init(a: Int, b: String) {
+    public init(a: Int, b: String) {
         self.a = a
         self.b = b
     }
 
-    func show() {
+    public func show() {
         print("a: \(a), b: \(b)")
     }
 }
